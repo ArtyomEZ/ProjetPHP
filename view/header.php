e673f26e9f1b8ed3fdd0ba5abc6ac8e3a76c6a28
Alexis B. <154525045+JebusAB@users.noreply.github.com>
2024-11-28 13:06:16 +0100

    added views & reorganised

---
+++ view/header.php
@@ -0,0 +1,24 @@
+
+	<div class="header-style">
+		<div class="row">
+			<div class="column">
+				<a href="">
+					<p>Paramètres</p>
+				</a>
+			</div>
+			<div class="column">
+				<p>Mes infos</p>
+			</div>
+			<div class="column">
+			</div>
+			<div class="column">
+			</div>
+			<div class="column">
+			</div>
+			<div class="column">
+			</div>
+			<div class="column-end">
+				<img src="../ressources/FSI_logo.png" alt="Logo FSI">
+			</div>
+		</div>
+	</div>
---
+++ view/pageConnexion.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../ressources/style.css">
+    <title>Connexion</title>
+</head>
+<body class="connexion">
+    <div class="row">
+        <div class="column">
+            <div class="form-style">
+                <form>
+                    <div class="row">
+                        <div class="column">
+                            <label>Identifiant:</label>
+                        </div>
+                        <div class="column">
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="column">
+                            <label>Mot de Passe:</label>
+                        </div>
+                        <div class="column">
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <input type="submit" value="Confirmer">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="column">
+            <img src="../ressources/FSI_logo.png" alt="Logo FSI" class="img-30pc">
+        </div>
+    </div>
+</body>
+</html>
