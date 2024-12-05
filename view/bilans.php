fa6710b8b78cd4b82a3b714ffa07a9a7c1c776bc
Alexis B. <154525045+JebusAB@users.noreply.github.com>
2024-12-05 16:52:47 +0100

    création des pages d'Acceuil etudiant et admin

---
+++ view/bilans.php
@@ -0,0 +1,31 @@
+<div class="content">
+    <h1>
+        Bilans
+    </h1>
+    <div class="greybox">
+        <div class="row">
+            <div class="column">
+                <div class="whitebox">
+                    <h2>
+                        Placeholder
+                    </h2>
+                    <form action="">
+                        <input type="submit">
+                    </form>
+                </div>
+            </div>
+            <div class="column">
+            </div>
+            <div class="column">
+                <div class="whitebox">
+                    <h2>
+                        Placeholder
+                    </h2>
+                    <form action="">
+                        <input type="submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
