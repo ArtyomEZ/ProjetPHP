d56716b51a7991f06d2dff4ae318715f21892a1f
Alexis B. <154525045+JebusAB@users.noreply.github.com>
2024-12-06 11:04:00 +0100

    dernier push avant passage

--- view/PageBilan.php
+++ view/PageBilan.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../ressources/style.css">
-    <title>Bilan</title>
-</head>
-<body>
-    <?php
-        include("header.php");
-        include("tableauBilan.php");
-    ?>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../ressources/style.css">
+        <title>Bilan</title>
+    </head>
+    <body>
+        <?php
+            include("header.php");
+            include("tableauBilan.php");
+        ?>
+    </body>
 </html>
