1383e1a2fa7e8c1fe5e8358fe777561141a2fe7a
Alexis B. <154525045+JebusAB@users.noreply.github.com>
2024-12-06 11:00:49 +0100

    création des pages d'Acceuil etudiant et admin

--- view/bilans.php
+++ view/bilans.php
@@ -6,24 +6,32 @@
         <div class="row">
             <div class="column">
                 <div class="whitebox">
+                    <br>
                     <h2>
                         Bilan 1
                     </h2>
+                    <br>
+                    <br>
                     <form action="">
                         <input type="submit" class="sub-center" value="Voir">
                     </form>
+                    <br>
                 </div>
             </div>
             <div class="column">
             </div>
             <div class="column">
                 <div class="whitebox">
+                    <br>
                     <h2>
                         Bilan 2
                     </h2>
+                    <br>
+                    <br>
                     <form action="">
                         <input type="submit" class="sub-center" value="Voir">
                     </form>
+                    <br>
                 </div>
             </div>
         </div>
