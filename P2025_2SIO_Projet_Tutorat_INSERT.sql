3d3c01f4a0c5f0cce60ea062324c01e8377fd204
yanisb012 <39186298+yanisb012@users.noreply.github.com>
2024-12-06 10:31:39 +0100

    Update P2025_2SIO_Projet_Tutorat_INSERT.sql

--- P2025_2SIO_Projet_Tutorat_INSERT.sql
+++ P2025_2SIO_Projet_Tutorat_INSERT.sql
@@ -1,10 +1,9 @@
     INSERT INTO Specialite (nomSpe) VALUES
     ('Informatique'),
     ('Mathématiques'),
-    ('Francais'),
-    ('Economie');
+    ('Francais');
 
-    INSERT INTO Type_Utilisateur (typUser) VALUES
+    INSERT INTO TypeUtilisateur (typUser) VALUES
     ('Étudiant'),
     ('Tuteur'),
     ('Administrateur');
@@ -12,34 +11,47 @@
     INSERT INTO Entreprise (nomEnt, adrEnt, cpEnt, vilEnt) VALUES
     ('TechInnov', '12 Rue des Inventions','75008', 'Paris'),
     ('BioLab Sciences', '8 Avenue de la Recherche', '69100', 'Villeurbanne'),
-    ('Mécatronix', '22 Rue des Ingénieurs','31000', 'Toulouse');
+    ('Cabottech', '5 Rue des cabots', '69002', 'Lyon'),
+    ('ChickenSpot', '10 Boulevard des poulets', '69003', 'Lyon');
 
     INSERT INTO Classe (nomCla, maxEtuCla) VALUES
     ('3 Olen', 30),
     ('4 Olen', 25),
     ('5 Olen', 20);
 
-    INSERT INTO Maitre_Apprentissage (nomMai, preMai, telMai, mailMai, idEnt) VALUES
+    INSERT INTO MaitreApprentissage (nomMai, preMai, telMai, mailMai, idEnt) VALUES
     ('Dupont', 'Jean', '0612345678', 'jean.dupont@techinnov.com', 1),
-    ('Martin', 'Claire', '0623456789', 'claire.martin@biolab.com', 2),
-    ('Leclerc', 'Paul', '0634567890', 'paul.leclerc@mecatronix.com', 3);
+    ('Martin', 'Claire', '0623456789', 'claire.martin@biolab.com', 1),
+    ('Cabot', 'Robin', '0634567890', 'robin.cabot@cabottech.com', 2),
+    ('Sanders','Colonel', '0645678901', 'colonel.sanders@chickenspot.com', 3);
 
     INSERT INTO Utilisateur (nomUti, preUti, mailUti, telUti, adrUti, cpUti, vilUti, logUti, mdpUti, altUti, nbrMaxEtu3, nbrMaxEtu4, nbrMaxEtu5, idTypUser, idSpe, idCla, idEnt, idMai, idTut) VALUES
-    ('Dubois', 'Alice', 'alice.dubois@etu.univ.fr', '0701234567', '10 Rue des Écoles', '75006', 'Paris', 'alice.dubois', 'password123', 1, 30, 25, 20, 1, 1, 1, 1, 1, NULL),
-    ('Morel', 'Benoît', 'benoit.morel@etu.univ.fr', '0712345678', '14 Rue des Étudiants', '69007', 'Lyon', 'benoit.morel', 'secure456', 0, 30, 25, 20, 2, NULL, 1, 1,NULL,NULL),
-    ('Lemoine', 'Chloé', 'chloe.lemoine@etu.univ.fr', '0723456789', '18 Avenue des Sciences', '34000', 'Montpellier', 'chloe.lemoine', 'mysecret789', 0,  30, 25, 20, 3, NULL, NULL, NULL,NULL,NULL);
+    ('Bouquet', 'Yanis', 'yanis.bouquet@lyon.ort.fr', '0701231567', '4 rue des admins', '75002', '¨Paris', 'admin', 'admin123', NULL,NULL,NULL,NULL,3, NULL, NULL, NULL, NULL, NULL),
+    ('Goudet', 'Magali', 'magali.goudet@lyon.ort.fr', '0701254567', '8 Rue des tuteurs', '69002', 'Lyon', 'magali.goudet', 'magali123',NULL, 20,20,20,2, NULL, NULL, NULL, NULL, NULL),
+    ('Dupont', 'Arnaud', 'arnaud.dupont@lyon.ort.fr', '0712346678', '9 rue des tuteurs', '69002', 'Lyon', 'arnaud.dupont', 'arnaud123',NULL,20,20,20,2, NULL, NULL, NULL, NULL, NULL),
+    ('Rahaimi', 'Oualid', 'oualid.rahaimi@lyon.ort.fr', '0712345478', '10 rue des tuteurs', '69002', 'Lyon', 'oualid.rahaimi', 'oualid123', NULL, 20, 20, 20, 2, NULL, NULL, NULL, NULL, NULL),
+    ('Gharbi', 'Aymen', 'aymen.gharbi@lyon.ort.fr', '0701234567', '10 Rue des Écoles', '69008', 'Lyon', 'aymen.gharbi', 'aymen123', 1, NULL, NULL, NULL, 1, 1, 1, 1, 1, NULL),
+    ('Zenasni', 'Hakim', 'hakim.zenasni@lyon.ort.fr', '0712345678', '14 Rue des Étudiants', '69007', 'Lyon', 'hakim.zenasni', 'hakim123', 0, NULL, NULL, NULL, 1, 1, 1, 1, 2, 1),
+    ('Dupont', 'Dupond', 'dupont.dupont@lyon.ort.fr', '07012364517', '10 Rue des dupont', '69008', 'Lyon', 'dupont.dupond', 'dupont123', 1, NULL, NULL, NULL, 1, 2, 2, 2, 2, 2),
+    ('Dupont', 'Xavier', 'xavier.dupont@lyon.ort.fr', '0712345677', '18 Rue des recherche', '69007', 'Lyon', 'xavier.dupont', 'xavier123', 0, NULL, NULL, NULL, 1, 1, 1, NULL, 3, 3),
+    ('Lemoine', 'Chloé', 'chloe.lemoine@lyon.ort.fr', '0719876543', '12 Rue des Lumières', '69001', 'Lyon', 'chloe.lemoine', 'chloe123', 1, NULL, NULL, NULL, 1, 3, 2, 1, 2, 1),
+    ('Morel', 'Benoît', 'benoit.morel@lyon.ort.fr', '0714567890', '7 Rue des Sciences', '69002', 'Lyon', 'benoit.morel', 'benoit123', 1, NULL, NULL, NULL, 1, 1, 3, 2, 3, 1);
 
     INSERT INTO Bilan1 (notEnt1, notDos1, notOral1, rema1, datBil1, idUti) VALUES
-    (16.5, 15.0, 16.0, 'Incroyable.', '2024-05-15', 1),
-    (14.0, 13.0, 12.5, 'Bon travail.', '2024-06-10', 2),
-    (10.0, 9.0, 8.5, 'Peut mieux faire.', '2024-07-20', 3);
+    (16.5, 15.0, 16.0, 'Incroyable.', '2025-01-01', 5),
+    (14.0, 13.0, 12.5, 'Bon travail.', '2025-01-01', 6),
+    (10.0, 9.0, 8.5, 'Peut mieux faire.', '2025-01-01', 7),
+    (20.0, 20.0, 20.0, 'Excellent.', '2025-01-01', 8),
+    (8.0, 19.0, 1.5, 'Montagnes russes.', '2025-01-01', 9),
+    (5.0, 11.0, 13.0, 'Ensemble correct.', '2025-01-01', 10);
 
     INSERT INTO Bilan2 (notDos2, notOral2, rema2, sujMem, datBil2, idUti) VALUES
-    (15.0, 14.5, 'Bon travail global.', 'Optimisation d''algorithmes', '2024-09-15', 1),
-    (13.0, 12.5, 'Peut approfondir.', 'Analyse statistique', '2024-10-10', 2),
-    (17.0, 16.5, 'Excellent niveau.', 'Biotechnologies', '2024-11-20', 3);
+    (15.0, 18.5, 'Tres bon travail.', 'Optimisation algorithmes', '2025-06-01', 5),
+    (13.0, 12.5, 'Peut approfondir.', 'Analyse statistique', '2025-06-01', 6),
+    (7.0, 4.5, 'A revoir.', 'Biotechnologies', '2025-06-01', 7),
+    (19.5, 18.5, 'Excellent.', 'SQL avancé', '2025-06-01', 8),
+    (8.0, 10.5, 'Moyen.', 'Questionnement psychique', '2025-06-01', 9),
+    (10.0, 12.5, 'Correct.', 'PHP avancé', '2025-06-01', 10);
 
     INSERT INTO Alerte (datLim1, datLim2) VALUES
-    ('2024-11-01', '2024-11-15'),
-    ('2024-12-01', '2024-12-10'),
-    ('2024-12-15', '2024-12-20');
+    ('2025-01-15', '2025-06-15');
