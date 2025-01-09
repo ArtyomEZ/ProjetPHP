5d955c079570308c414858a26e7482a76d70b627
Alexis B. <154525045+JebusAB@users.noreply.github.com>
2025-01-09 09:39:11 +0100

    changé pageAcceuilAdmin.css et pageAccueuilAdmin.php pour être plus ressemblant à la maquette

--- view/pageAccueuilAdmin.php
+++ view/pageAccueuilAdmin.php
@@ -1,17 +1,26 @@
+<?php
+require_once 'init.php';
+
+if (isset($_SESSION['login']) || $_SESSION['role'] !== 'admin') {
+    $error = 'Permissions insuffisantes.';
+    header('Location: ../vue/pageConnexion.php?error='.($error));
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../ressources/style.css">
-        <title>Accueil</title>
-    </head>
-    <body>
-        <?php
-            include("../view/headerTuteur.php");
-            include("../view/alertes.php");
-            include("../view/listeElevesAdmin.php");
-            include("../view/parametresAdmin.php");
-        ?>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/pageAccueilAdm.css">
+    <title>Accueil</title>
+</head>
+<body>
+<?php
+include("../vue/headerAdmin.php");
+include("../vue/alertesAdmin.php");
+include("../vue/listeEleves.php");
+?>
+<a href="parametreAdmin.php">Paramètre générale</a>
+</body>
 </html>
